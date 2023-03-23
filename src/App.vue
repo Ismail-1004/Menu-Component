471a7f26535d898626c1f5188e3f2576283c0496
Ismail <herheulidzeismail980@gmail.com>
2023-03-23 13:17:35 +0500

    menu component created

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
+      integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
+      crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <Menu :logo="true">
+      <template v-slot:primary>
+        <ul class="menu__list d-flex">
+          <li class="menu__list-item">
+            <router-link to="/" class="menu__list-link standard"> More Apps </router-link>
+          </li>
+          <li class="menu__list-item">
+            <router-link to="/" class="menu__list-link standard"> Games </router-link>
+          </li>
+        </ul>
+      </template>
+
+      <template v-slot:secondary>
+        <ul class="menu__list d-flex">
+          <li class="menu__list-item standard__icon">
+            <router-link to=""> <i class="fa-sharp fa-solid fa-chart-simple"></i> </router-link>
+          </li>
+          <li class="menu__list-item standard__icon">
+            <router-link to=""> <i class="fa-solid fa-user"></i> </router-link>
+          </li>
+          <li class="menu__list-item standard__icon danger">
+            <i class="fa-solid fa-right-from-bracket"></i>
+          </li>
+        </ul>
+      </template>
+    </Menu>
+  </div>
+</template>
+
+<script>
+import Menu from './components/Menu.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Menu
+  }
+}
+</script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  text-decoration: none;
+  box-sizing: border-box;
+
+  font-family: sans-serif;
+}
+</style>
---
+++ src/plugin.js
@@ -0,0 +1,23 @@
+import Menu from './components/Menu.vue'
+
+export function install(Vue) {
+  if (install.installed) return;
+  install.installed = true;
+  Vue.component('Menu', Menu);
+}
+
+const plugin = {
+  install
+};
+
+let GlobalVue = null;
+if (typeof window !== 'undefined') {
+  GlobalVue = window.Vue;
+} else if (typeof global !== 'undefined') {
+  GlobalVue = global.Vue;
+}
+if (GlobalVue) {
+  GlobalVue.use(plugin);
+}
+
+export default Menu;
